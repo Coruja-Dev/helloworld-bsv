40cb098eaa0280d01472b4eaca5e9d0515dd87b8
CorujaDev <guilhermepdl97@protonmail.com>
2025-05-13 23:42:18 -0300

    Initial config done

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Helloworld } from './contracts/helloworld';
+import artifact from './contracts/artifacts/helloworld.json';
+Helloworld.loadArtifact(artifact);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
